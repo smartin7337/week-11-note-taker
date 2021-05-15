cc7046e7b4ba1fefc72514794098e572071488c8
Vossie <sv7337@outlook.com>
2021-05-14 20:56:29 -0400

    updating file structure in VS code

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const notesDB = require('./db/db.json')
 const app = express();
+const port = 3001
+const fs = require('fs');
 
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('./public'));
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Listening to port ${port}.`)
+})
