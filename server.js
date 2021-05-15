c84fdf86b1fb3e7b0b1dfccac349c920dc5c0aec
Vossie <sv7337@outlook.com>
2021-05-14 21:10:07 -0400

    notes route added

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
-const express = require('express');
-const notesDB = require('./db/db.json')
+const express = require("express");
+const notesDB = require("./db/db.json");
 const app = express();
-const port = 3001
-const fs = require('fs');
+const port = 3001;
+const fs = require("fs");
 
-
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
+app.get("/", function (request, response) {
+  response.sendFile("./public/index.html");
+});
 
-
-
-
-
-
-
-
-
-
+app.get("/notes", function (request, response) {
+  response.sendFile("./public/notes.html");
+});
 
 app.listen(port, () => {
-    console.log(`Listening to port ${port}.`)
-})
+  console.log(`Listening to port ${port}.`);
+});
